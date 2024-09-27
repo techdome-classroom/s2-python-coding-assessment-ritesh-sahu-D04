c3eeaca07ebd391b83ba44119a3408e1d8fafc12
ritest-sahu-D04 <ritsahu1@gmail.com>
2024-09-27 14:45:39 +0530

    Both the codes are completed

--- program1.py
+++ program1.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+       
+        bracket_map = {')': '(', '}': '{', ']': '['}
+        stack = []
 
+        for char in s:
+            if char in bracket_map.values():  
+                stack.append(char)
+            elif char in bracket_map.keys():  
+                if stack and stack[-1] == bracket_map[char]:
+                    stack.pop()  
+                else:
+                    return False  
 
-
-
-
-
-
-    
-
-
-
-  
-
+        return len(stack) == 0  
--- program2.py
+++ program2.py
@@ -1,10 +1,22 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
-
-
-
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        for i in range(n):
+            if i < n - 1 and roman_values[s[i]] < roman_values[s[i + 1]]:
+                total -= roman_values[s[i]]
+            else:
+                total += roman_values[s[i]]
+        
+        return total
